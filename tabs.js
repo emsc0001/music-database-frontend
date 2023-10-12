b7161134129b88d8a7986b619dfbc1f1bbd78438
Abdiox <abdirahmanroble2610@gmail.com>
2023-10-12 14:21:57 +0200

    udkommenteret tabs og lavet lidt html

--- tabs.js
+++ tabs.js
@@ -1,28 +1,28 @@
-let selectedTab = null;
+// let selectedTab = null;
 
-function initTabs() {
-  // Setup tab-toggling
-  document.querySelectorAll("#tabs h2").forEach((tab) => tab.addEventListener("click", selectTab));
-  // Click the first tab to enable everything
-  document.querySelector("#tabs h2").click();
-}
+// function initTabs() {
+//   // Setup tab-toggling
+//   document.querySelectorAll("#tabs h2").forEach((tab) => tab.addEventListener("click", selectTab));
+//   // Click the first tab to enable everything
+//   document.querySelector("#tabs h2").click();
+// }
 
-function selectTab(event) {
-  const tab = event.target;
-  // Only accept click if the tab isn't selected
-  if (!tab.classList.contains("selected")) {
-    // Unselect the last tab - if any
-    if (selectedTab) {
-      selectedTab.classList.remove("selected");
-      document.querySelector(`#${selectedTab.dataset.tabShow}`).classList.add("hide");
-    }
-    // Select this tab
-    tab.classList.add("selected");
-    document.querySelector(`#${tab.dataset.tabShow}`).classList.remove("hide");
+// function selectTab(event) {
+//   const tab = event.target;
+//   // Only accept click if the tab isn't selected
+//   if (!tab.classList.contains("selected")) {
+//     // Unselect the last tab - if any
+//     if (selectedTab) {
+//       selectedTab.classList.remove("selected");
+//       document.querySelector(`#${selectedTab.dataset.tabShow}`).classList.add("hide");
+//     }
+//     // Select this tab
+//     tab.classList.add("selected");
+//     document.querySelector(`#${tab.dataset.tabShow}`).classList.remove("hide");
 
-    // Remember the selected tab
-    selectedTab = tab;
-  }
-}
+//     // Remember the selected tab
+//     selectedTab = tab;
+//   }
+// }
 
-export { initTabs };
+// export { initTabs };
